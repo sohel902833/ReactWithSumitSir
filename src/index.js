a1290910692bb531590d47c84cfcaeb65b9d2a83
sohel902833 <mdsohrabhossainsohel902802@gmail.com>
2021-07-19 22:50:50 -0700

    part 7 end

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
 import React from "react";
 import ReactDom from "react-dom";
-// const element=React.createElement('h1',null,"Hello World");
+import Clock from './Clock'
 
-setInterval(() => {
-  const element = (
-    <div>
-      <h1 className="Hello World..">
-        <p>Md Sohrab Hossain Sohel {new Date().toLocaleTimeString()}</p>
-        <p>Md Sohrab Hossain Sohel</p>
-      </h1>
-    </div>
-  );
-  ReactDom.render(element,document.getElementById("root")); 
-}, 1000);
+
+
+ReactDom.render(<Clock locale="bn-BD"/>,document.getElementById("root")); 
+
